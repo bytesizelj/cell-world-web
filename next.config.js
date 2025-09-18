ca8d59dc3b9cae801a617b5930ef9fad9a5713a1
Leslieanna Joseph <bytesofknowledgelj@gmail.com>
2025-09-17 22:40:19 -0400

    Add cache headers to prevent browser caching issues

--- next.config.js
+++ next.config.js
@@ -3,6 +3,27 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/images/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=0, must-revalidate',
+          },
+        ],
+      },
+    ]
+  },
 }
-
 module.exports = nextConfig
