1c69b9d66b0da5a25ccb28ac40e8552b8c324629
Leslieanna Joseph <bytesofknowledgelj@gmail.com>
2025-10-27 11:31:26 -0400

    Fix duplicate body tags causing page duplication

--- app/layout.tsx
+++ app/layout.tsx
@@ -77,23 +77,8 @@ export default function RootLayout({
           `}
         </Script>
         {children}
-        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-E9RDJE166F"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-E9RDJE166F');
-          `}
-        </Script>
-        {children}
-        <Analytics />  {/* ADD THIS LINE */}
-      </body>
-      </body>
+        <Analytics />  
+       </body>
     </html>
   );
 }
